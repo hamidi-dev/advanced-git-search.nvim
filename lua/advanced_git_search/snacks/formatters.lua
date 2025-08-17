37e4b6f123be7de98643b3a3e61eb12889608e2b
Mohamed Hamidi <mohamed@hamidi.dev>
2025-08-17 09:13:19 +0200

    feat: add "both" option to "entry_default_author_or_date

--- lua/advanced_git_search/snacks/formatters.lua
+++ lua/advanced_git_search/snacks/formatters.lua
@@ -1,23 +1,43 @@
 local M = {}
+local config = require("advanced_git_search.utils.config")
 
 ---@return snacks.picker.format
 M.git_log = function()
     return function(item, picker)
         local a = Snacks.picker.util.align
+        local mode = config.entry_default_author_or_date()
 
         local ret = {} ---@type snacks.picker.Highlight[]
-        ret[#ret + 1] =
-            { picker.opts.icons.git.commit, "SnacksPickerGitCommit" }
+        ret[#ret + 1] = { picker.opts.icons.git.commit, "SnacksPickerGitCommit" }
         ret[#ret + 1] = {
             a(item.commit, 8, { truncate = true }),
             "SnacksPickerGitCommit",
         }
         ret[#ret + 1] = { " " }
-        ret[#ret + 1] = {
-            a(item.author, 15, { truncate = true }),
-            "SnacksPickerGitDate",
-        }
-        ret[#ret + 1] = { " " }
+        if mode == "date" then
+            ret[#ret + 1] = {
+                a(item.date, 10, { truncate = true }),
+                "SnacksPickerGitDate",
+            }
+            ret[#ret + 1] = { " " }
+        elseif mode == "both" then
+            ret[#ret + 1] = {
+                a(item.date, 10, { truncate = true }),
+                "SnacksPickerGitDate",
+            }
+            ret[#ret + 1] = { " " }
+            ret[#ret + 1] = {
+                a(item.author, 15, { truncate = true }),
+                "SnacksPickerGitDate",
+            }
+            ret[#ret + 1] = { " " }
+        else
+            ret[#ret + 1] = {
+                a(item.author, 15, { truncate = true }),
+                "SnacksPickerGitDate",
+            }
+            ret[#ret + 1] = { " " }
+        end
         ret[#ret + 1] = { item.msg, "SnacksPickerGitMsg" }
         return ret
     end
--- lua/advanced_git_search/telescope/finders/utils.lua
+++ lua/advanced_git_search/telescope/finders/utils.lua
@@ -2,15 +2,27 @@ local utils = require("advanced_git_search.utils")
 local entry_display = require("telescope.pickers.entry_display")
 local config = require("advanced_git_search.utils.config")
 
-local show_date_instead_of_author = (
-    config.entry_default_author_or_date() == "date"
-)
+local display_mode
+
+local function get_display_mode()
+    if not display_mode then
+        display_mode = config.entry_default_author_or_date()
+    end
+    return display_mode
+end
 
 local M = {}
 local last_prompt = nil
 
 M.toggle_show_date_instead_of_author = function()
-    show_date_instead_of_author = not show_date_instead_of_author
+    local mode = get_display_mode()
+    if mode == "author" then
+        display_mode = "date"
+    elseif mode == "date" then
+        display_mode = "both"
+    else
+        display_mode = "author"
+    end
 end
 
 --- Parse "--format=%C(auto)%h %as %C(green)%an _ %Creset %s" to table
@@ -37,41 +49,61 @@ M.git_log_entry_maker = function(entry)
         end
     end
 
-    local second_width
-    if show_date_instead_of_author then
-        second_width = 10
-    else
-        second_width = #author
-    end
+    local displayer
+    local make_display
+    local mode = get_display_mode()
 
-    local displayer = entry_display.create({
-        separator = " ",
-        items = {
-            { width = 7 },
-            { width = second_width },
-            { remaining = true },
-        },
-    })
-
-    local make_display = function(display_entry)
-        if show_date_instead_of_author then
+    if mode == "both" then
+        displayer = entry_display.create({
+            separator = " ",
+            items = {
+                { width = 7 },
+                { width = 10 },
+                { width = #author },
+                { remaining = true },
+            },
+        })
+        make_display = function(display_entry)
             return displayer({
                 {
                     display_entry.opts.commit_hash,
                     "TelescopeResultsIdentifier",
                 },
                 { display_entry.opts.date, "TelescopeResultsVariable" },
-                { display_entry.opts.message, "TelescopeResultsConstant" },
-            })
-        else
-            return displayer({
-                {
-                    display_entry.opts.commit_hash,
-                    "TelescopeResultsIdentifier",
-                },
                 { display_entry.opts.author, "TelescopeResultsVariable" },
                 { display_entry.opts.message, "TelescopeResultsConstant" },
             })
+        end
+    else
+        local second_width = mode == "date" and 10 or #author
+        displayer = entry_display.create({
+            separator = " ",
+            items = {
+                { width = 7 },
+                { width = second_width },
+                { remaining = true },
+            },
+        })
+        make_display = function(display_entry)
+            if mode == "date" then
+                return displayer({
+                    {
+                        display_entry.opts.commit_hash,
+                        "TelescopeResultsIdentifier",
+                    },
+                    { display_entry.opts.date, "TelescopeResultsVariable" },
+                    { display_entry.opts.message, "TelescopeResultsConstant" },
+                })
+            else
+                return displayer({
+                    {
+                        display_entry.opts.commit_hash,
+                        "TelescopeResultsIdentifier",
+                    },
+                    { display_entry.opts.author, "TelescopeResultsVariable" },
+                    { display_entry.opts.message, "TelescopeResultsConstant" },
+                })
+            end
         end
     end
 
